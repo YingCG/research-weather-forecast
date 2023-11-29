8587b0300cf972a91757349bc6a7096da4d00d78
yingcg <yingchiugoh@gmail.com>
2023-11-30 04:43:20 +1300

    mt albert rain & soil moisture

--- PlantMode/client/src/App.js
+++ PlantMode/client/src/App.js
@@ -1,35 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-
-// function App() {
-//   const [data, setData] = useState([{}])
-
-//   useEffect(() => {
-//     fetch("/weathers").then(
-//       res => res.json()
-//     ).then(
-//       data => {
-//         setData(data)
-//         console.log(data)
-//       }
-//     )
-//   }, [])
-
-//   return (
-//     <div>
-//       {/* {data.weathers} */}
-//       {(typeof data.weathers === 'undefined') ? (
-//         <p>Loading...</p>
-//       ): (
-//         data.weathers.map((waterlevel, i) => (
-//           <p key={i}>{waterlevel}</p>
-//         ))
-//       )}
-//     </div>
-//   )
-// }
-
-// export default App
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -37,19 +5,21 @@ function App() {
     const [todayWeather, setTodayWeather] = useState({});
     const [pastWeekWeather, setPastWeekWeather] = useState([]);
     const [prediction, setPrediction] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         // Fetch today's weather data from the OpenWeatherMap API
         axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${process.env.API_KEY}`)
             .then(response => {
-                // Extract relevant data from the response, e.g., temperature and rainfall
                 const temperature = response.data.main.temp;
-                const rainfall = 0; // You need to get actual rainfall data from the API
+                const rainfall = 0; // Replace with actual rainfall data
 
                 setTodayWeather({ temperature, rainfall });
             })
             .catch(error => {
                 console.error(error);
+                setError("Error fetching today's weather data");
             });
 
         // Fetch past week's weather data from an API or your database
@@ -61,22 +31,38 @@ function App() {
 
     const predictWeather = async () => {
         try {
+            setLoading(true);
+
             // Send a POST request to your Flask API with today's and past week's weather data
             const response = await axios.post('/weathers', {
                 today: todayWeather,
                 past_week: pastWeekWeather
             });
+
             setPrediction(response.data.prediction);
         } catch (error) {
             console.error(error);
+            setError("Error predicting weather");
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
         <div>
             <h1>Weather Prediction</h1>
-            <button onClick={predictWeather}>Predict Weather</button>
-            {prediction && <p>Prediction: {prediction}</p>}
+            <button onClick={predictWeather} disabled={loading}>
+                {loading ? 'Predicting...' : 'Predict Weather'}
+            </button>
+
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+
+            {prediction && (
+                <div>
+                    <h2>Prediction: {prediction}</h2>
+                    {/* Display additional prediction details if available */}
+                </div>
+            )}
         </div>
     );
 }
